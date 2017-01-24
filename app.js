4e09d5677e8d0482e563b7bb50c1651fcc06eb2c
getvivekjoshi <getvivekjoshi@gmail.com>
2017-01-24 11:55:58 +0530

    assignement1

--- app.js
+++ app.js
@@ -3,46 +3,41 @@
 
  angular.module('LunchCheck', [])
   		.controller('LunchCheckController', LunchCheckController);
-
+		
 		 lunchCheckController.$inject = ['$scope'];
-
+		 
 		  function LunchCheckController ($scope) {
                  $scope.input = "";
 				 $scope.message = "";
-
+				 
 				 $scope.checkValue = function (){
-
+				 
 				 var array = $scope.input.split(',');
 				 var count=0;
 				 var item;
 				 array.forEach(function(item){
-
+				 
 				 	 	if(item===""){
-
-
-
+					
+					
+						
 					}else{
 						count++;
 					}
 				 });
-
+				
 				 if( count===0 ){
 					 $scope.message = "Please enter data first"
 				 }else{
-
+				 
 				 		 if (count <=3){
 					 $scope.message = "Enjoy!"
 					 } else if(count > 3){
 					  $scope.message = "Too much!"
 					 }
 				 }
-
-
-				 /*
-				 else if(array.length==0){
-				 	$scope.message = "test message!"
-				 }*/
+			
 				 };
          }
-
+		
 })();
