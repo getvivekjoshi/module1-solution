9f231f18d3e437374fe98a6f7c754038a209e34d
getvivekjoshi <getvivekjoshi@gmail.com>
2017-01-24 11:21:39 +0530

    assignment-1

---
+++ app.js
@@ -0,0 +1,48 @@
+(function (){
+'use strict';
+
+ angular.module('LunchCheck', [])
+  		.controller('LunchCheckController', LunchCheckController);
+
+		 lunchCheckController.$inject = ['$scope'];
+
+		  function LunchCheckController ($scope) {
+                 $scope.input = "";
+				 $scope.message = "";
+
+				 $scope.checkValue = function (){
+
+				 var array = $scope.input.split(',');
+				 var count=0;
+				 var item;
+				 array.forEach(function(item){
+
+				 	 	if(item===""){
+
+
+
+					}else{
+						count++;
+					}
+				 });
+
+				 if( count===0 ){
+					 $scope.message = "Please enter data first"
+				 }else{
+
+				 		 if (count <=3){
+					 $scope.message = "Enjoy!"
+					 } else if(count > 3){
+					  $scope.message = "Too much!"
+					 }
+				 }
+
+
+				 /*
+				 else if(array.length==0){
+				 	$scope.message = "test message!"
+				 }*/
+				 };
+         }
+
+})();
